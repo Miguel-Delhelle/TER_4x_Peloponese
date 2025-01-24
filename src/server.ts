f9a571eea03aeac7dcd170a0a977ed51b932202b
Miguel-Delhelle <miguel.delhelle@gmail.com>
2025-01-24 16:36:55 +0100

    TypeScript compilation manuelle et node ok

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+// chargement des modules node avec require()
+const cors = require("cors");
+var express = require("express");
+const url = require('url');
+//const querystring = require('querystring');
+//const bcrypt = require("bcrypt");
+//var mysql = require('mysql');
+
+// con pour connexion
+/*var con = mysql.createConnection({
+    host: "localhost",
+    user: "projetweb",
+    password: "Password1234_",
+    database: "PROJET_WEB"
+}); */
+
+//fs pour file system
+var fs = require("fs");
+
+// express démarre le serveur
+var app = express();
+app.listen(8888);
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', function (req:any, response:any) {
+    response.sendFile('index.html', { root: __dirname});
+});
