3809628b418a5602042a50f47d758bf054daa7b8
MIGUEL-DELHELLE <miguel.delhelle@gmail.com>
2025-03-05 10:34:18 +0100

    [TEST] Initialisation compliqué de phaser, help me

---
+++ Client_game/vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    // Configuration spécifique pour la construction du projet
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['phaser'],
+  },
+});
