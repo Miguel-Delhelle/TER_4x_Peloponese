d322b2d3281b02831b2033ff0d894e9b7b4896ae
Elunelly <luna.philip@etu.umontpellier.fr>
2025-03-13 13:04:23 +0100

    [NEW] Merging all MiniWorld assets into one file + WIP tilesets

---
+++ mapTiled/Tileset/ColoredBuildingsPreview.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="ColoredBuildingsPreview" tilewidth="16" tileheight="16" tilecount="780" columns="30">
+ <image source="MiniWorldSprites/ColoredBuildingsPreview.png" width="480" height="416"/>
+</tileset>
