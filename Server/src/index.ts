91a7e48e6fefe18bd6f112ecdf468faf518c5d31
MIGUEL-DELHELLE <miguel.delhelle@gmail.com>
2025-05-12 16:19:36 +0200

    beforeBuild

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,18 +1,47 @@
+import "reflect-metadata";
 import express from "express";
 import path from "path";
+import os from "os";
 import { fileURLToPath } from 'url';
+import { AppDataSource } from "./data-source"
+import { User } from "./entity/User"
+import { Database } from "sqlite3";
+
+
 const app = express();
 const port = "3000";
+const db = new Database('greekAnatomic.sqlite');
 
-// Obtenir le chemin absolu du fichier actuel
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+AppDataSource.initialize().then(async () => {
 
+    console.log("Inserting a new user into the database...")
+    const user = new User()
+    user.firstName = "Mathis"
+    user.lastName = "Rosier"
+    user.age = 24
+    await AppDataSource.manager.save(user)
+    console.log("Saved a new user with id: " + user.id)
+
+    console.log("Loading users from the database...")
+    const users = await AppDataSource.manager.find(User)
+    console.log("Loaded users: ", users)
+
+    console.log("Here you can setup and run express / fastify / any other framework.")
+
+}).catch(error => console.log(error))
 
 app.get("/", (req, res) => {
   //res.send("Hello World!");
   res.sendFile(path.resolve(__dirname,"..","..","Client","dist","index.html"));
 });
+
+app.get("/coucou", (req,res)=> {
+  res.status(200).json({
+    areYouInTruth:"yes",
+    whoAreYou:"My team enfin j'espère",
+    ipAdress: os.hostname()
+  })
+})
 
 app.use(express.static(path.resolve(__dirname, "../../Client/dist")));
 
