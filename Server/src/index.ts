1f8e5f965f5735bbad5098720b5fbc2db8dbb167
MIGUEL-DELHELLE <miguel.delhelle@gmail.com>
2025-05-12 17:51:10 +0200

    Type ORM OK, login, register ok

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 import { AppDataSource } from "./data-source"
 import { User } from "./entity/User"
 import { Database } from "sqlite3";
-
+import * as bcrypt from 'bcrypt';
 
 const app = express();
 const port = "3000";
@@ -14,19 +14,14 @@ const db = new Database('greekAnatomic.sqlite');
 
 AppDataSource.initialize().then(async () => {
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Mathis"
-    user.lastName = "Rosier"
-    user.age = 24
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+    //console.log("Inserting a new user into the database...")
+    //const user = new User("test@test.fr","testCoucou",await hashedPassword("k76"));
+    //await AppDataSource.manager.save(user)
+    //console.log("Saved a new user with id: " + user.id)
 
     console.log("Loading users from the database...")
     const users = await AppDataSource.manager.find(User)
     console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
 
 }).catch(error => console.log(error))
 
@@ -43,9 +38,65 @@ app.get("/coucou", (req,res)=> {
   })
 })
 
+app.use(express.json());
+
 app.use(express.static(path.resolve(__dirname, "../../Client/dist")));
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+
+app.post('/register', async function(req,response){
+  let mail:string = req.body.mail;
+  let username:string = req.body.username;
+  let password:string = req.body.password;
+
+  let userRegister:User = new User(mail,username,await hashedPassword(password));
+
+  await AppDataSource.manager.save(userRegister);
+
+  response.status(200).json("Inscription réussi");
+});
+
+app.post('/login', async function(req,response) {
+let mailReq = req.body.mail;
+let password = req.body.password;
+
+try {
+  let userLogins = await AppDataSource.manager.findBy(User, {mail: mailReq});
+  let userLogin = userLogins[0];
+
+  let passwordOk:boolean = await verifyPassword(password, userLogin._hashedPassword);
+
+  if (passwordOk){
+    delete userLogin.hashedPassword;
+    response.status(200).json(userLogin);
+  }else{
+    response.status(403).json({ error: "Mot de passe invalide"});
+  }
+  
+} catch (error) {
+  response.status(500).json({ error: 'Erreur interne au serveur' });
+}
+
+});
+
+
+
+async function hashedPassword(clearPassword:string):Promise<string> {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(clearPassword, salt);
+  return hashedPassword;
+};
+
+async function verifyPassword(clearPassword, hashedPassword) {
+try {
+    const match = await bcrypt.compare(clearPassword, hashedPassword);
+    return match; // Retourne true si les mots de passe correspondent, false sinon
+} catch (error) {
+    throw new Error('Erreur lors de la vérification du mot de passe');
+}
+}
