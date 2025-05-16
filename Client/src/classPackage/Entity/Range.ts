b4f7ab8d91fa22cd2e2a26f536ffc2f5d574af3b
MIGUEL-DELHELLE <miguel.delhelle@gmail.com>
2025-05-16 20:40:08 +0200

    [WIP] class non implémenté commenté pour éviter les erreurs

--- Client/src/classPackage/Entity/Range.ts
+++ Client/src/classPackage/Entity/Range.ts
@@ -1,7 +1,13 @@
 import { Unit } from "./Unit";
 
-export class Range extends Unit{
+/*export class Range extends Unit{
+   
 
    constructor();
 
+   fight(): void {
+      throw new Error("Method not implemented.");
+   }
+
 }
+*/
